7e28144846d4c4e66f37a45299308afd5daa677d
efati geovani <127381007+efatigeovaniisir@users.noreply.github.com>
2023-09-22 19:57:56 +0900

    hasil belajar pertemuan - 5

--- studi-kasus.php
+++ studi-kasus.php
@@ -3,7 +3,9 @@ for($i = 1; $i <= 100; $i++)
 {
     if($i % 2 == 1 )
     {
-        echo $i . "<br>";
+        echo $i . "Bilangan Ganjil <br>";
+    }else{
+        echo $i . "Bilangan Genap <br>";
     }
 }
 ?>
